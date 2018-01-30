4973a643fc5aab6d69d7276a439636409b567a85
AdelineClere <AdelineClere@users.noreply.github.com>
2018-01-30 17:10:25 +0100

    Travail 30/01/18

--- JQUERY/assets/js/08.hugo.js
+++ JQUERY/assets/js/08.hugo.js
@@ -1,24 +1,23 @@
 // -- Initialisation de jQuery (DOM READY)
 $(function() {
 
-
-    // -- Tableau indexé d'objet Contact (= transforme le tableau html en T. indexé)
+    // -- Tableau indexé d'objet Contact
     var CollectionDeContacts = [];
 
-    /*--------------------------------------------------------- 2.
-            DECLARATION DES FONCTIONS
-    ____________________________________________________________*/
+    /* --------------------------------------------------------------
+                        DECLARATION DES FONCTIONS
+    -------------------------------------------------------------- */
 
     function ajouterContact(Contact) {
 
-        // -- Ajouter "contact" dans "CollectionDeContacts"
+        // -- Ajouter "Contact" dans "CollectionDeContacts"
         CollectionDeContacts.push(Contact);
         console.log(CollectionDeContacts);
-
+        
         // -- On cache la phrase : Aucun Contact.
         $('.aucuncontact').hide();
 
-        // -- Mise à jour du HTML ( `` = pour écrire du text et html)
+        // -- Mise à jour du HTML
         $(`
             <tr>
                 <td>` + Contact.nom + `</td>
@@ -33,31 +32,31 @@ $(function() {
 
         // -- Affiche une Notification
         $('.alert-contact').fadeIn().delay(4000).fadeOut();
+
     }
 
     function reinitialisationDuFormulaire() {
-        // -- En jQuery
+        //  -- En jQuery 
         $('#contact').trigger('reset');
         $('#contact').get(0).reset();
         $('#contact .form-control').val('');
-
-        // -- En Javascript
+        
+        // -- En Javascript :
         document.getElementById('contact').reset();
     }
 
-
-    function validateEmail (email) {
+    function validateEmail(email){
         var emailReg = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
         var valid = emailReg.test(email);
     
         if(!valid) {
-            return false; 
+            return false;
         } else {
             return true;
         }
     }
     
-    function validateTel (tel) {
+    var validateTel = tel => {
         var telReg = new RegExp("(0|\\+33|0033)[1-9][0-9]{8}");
         var valid = telReg.test(tel);
     
@@ -68,34 +67,36 @@ $(function() {
         }
     }
 
-    // -- Fonction pour vérifier qu'un contact est déjà présent
     function unContactEstPresent(Contact) {
-  
-        // -- Booléen qui indique la présence d'un contact dans ma collection
+
+        // -- Booleen qui indique la présence d'un contact dans ma collection
         let estPresent = false;
 
         // -- On parcourt le tableau à la recherche d'une correspondance
-        for(let i= 0 ; CollectionDeContacts.length ; i++) {
+        for(let i = 0 ; i < CollectionDeContacts.length ; i++) {
 
             if(Contact.email === CollectionDeContacts[i].email) {
                 // -- Si une correspondance est trouvé "estPresent" passe à VRAI (true)
-                estPresent = true ;
+                estPresent = true;
                 // -- On arrête la boucle, plus besoin de poursuivre.
                 break;
             }
+
         }
+
         // -- On retourne le booleen
         return estPresent;
+
     }
 
-/* --------------------------------------------------------------
+    /* --------------------------------------------------------------
                     TRAITEMENT DE MON FORMULAIRE
--------------------------------------------------------------- */
+    -------------------------------------------------------------- */
 
-// -- Détection de la soumission de mon formulaire
+    // -- Détection de la soumission de mon formulaire
     $('#contact').on('submit', function(e) {
 
-        // -- Stopper la redirection dela pg
+        // -- Stopper la redirection de la page
         e.preventDefault();
 
         // -- Récupération des champs à vérifier
@@ -105,25 +106,26 @@ $(function() {
         email   = $('#email');
         tel     = $('#tel');
 
-         // -- Vérification des informations
+        // -- Vérification des informations
         let mesInformationsSontValides = true;
 
         // -- Vérification du Prénom
         if(prenom.val().length === 0) {
-            // -- Les champs est incorrect, car il n'a pas été rempli
-            mesInformationsSontValides = false;
-        }
-        // -- Vérification du Nom
-        if(nom.val().length === 0) {
-            // -- Le champ est incorrect, car il n'a pas été rempli
+            // -- Le champ est incorrect, car il n'a pas été rempli...
             mesInformationsSontValides = false;
         }
 
-        // -- Vérification du Mail et du Tel
+        // -- Vérification du Nom
+        if(nom.val().length === 0) {
+            // -- Le champ est incorrect, car il n'a pas été rempli...
+            mesInformationsSontValides = false;
+        }
+
+        // -- Vérification du Mail
         if(!validateEmail(email.val())) {
             mesInformationsSontValides = false;
         }
-         
+
         // -- Vérification du Tel
         if(!validateTel(tel.val())) {
             mesInformationsSontValides = false;
@@ -133,18 +135,18 @@ $(function() {
 
             // -- Tout est correct
             let Contact = {
-                //clé      //valeur
+                //cle       //valeur
                 nom         : nom.val(),
                 prenom      : prenom.val(),
                 email       : email.val(),
-                tel         : tel.val(),
+                tel         : tel.val()
             };
 
             /**
-            * Si le contact est présent dans la collection, on informe l'utilisateur
-            * sinon, on peut procéder à la suite du traitement.
-            */
-            if(unContactEstPresent(contact)) {
+             * Si le contact est présent dans la collection, on informe l'utilisateur
+             * Sinon, on peut procéder à la suite du traitement.
+             */
+            if(unContactEstPresent(Contact)) {
                 reinitialisationDuFormulaire();
                 alert('ATTENTION\nCe contact est déjà présent !');
             } else {
@@ -154,11 +156,10 @@ $(function() {
         } else {
 
             // -- Tous les champs n'ont pas été remplis.
-            alert('ATTENTION\nVeuillez bien remplir tous les champs.'); // \n = saut de ligne
-
+            alert('ATTENTION\nVeuillez bien remplir tous les champs.');
 
         }
- 
-    });
 
-});
+    })
+
+})
